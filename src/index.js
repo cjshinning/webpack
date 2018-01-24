853bd328e1fbdbb371252deea6dfcd5b7b3b9219
cjshinning <784319417@qq.com>
2018-01-24 23:01:58 +0800

    webpack基本配置

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello World!!!</h1>,
+    document.getElementById('example')
+);
+
+// console.log(111)
